c6bc3c0d7968aa4534bb70bf2f00b19665792cec
League Student <league@iMac-8.attlocal.net>
2019-09-15 12:58:13 -0700

    EPICK

--- webgl_demo.js
+++ webgl_demo.js
@@ -5,8 +5,10 @@ var shaderProgram;
 
 var vertexArray;
 var vertexBuffer;
-
+var iindexBuffer;
 var positionAttribID;
+var uvCoordID
+var checkTexture;
 
 window.onload = function(){
     canvas = document.getElementById("canvasID");
@@ -26,21 +28,46 @@ window.onload = function(){
     gl.useProgram(shaderProgram);
 
     let vertices = [
-        -0.5, -0.5, 0.0, 
-        -0.5, 0.5, 0.0,
-         0.5, 0.5, 0.0
+        -0.5, -0.5, 0.0, 0.0,0.0,
+        -0.5, 0.5, 0.0, 0.0, 1.0,
+         0.5, 0.5, 0.0, 1,0 ,1.0,
+         0.5,-0.5,0.0,1.0,0.0
     ];
+
+    let indices = [
+        0,1,2,2,3,0
+    ];
+
+    let texPixels = [
+        0,0,0,255, 255,255,255,255,
+        255,255,255,255, 0,0,0,255
+    ]
+    
 
     vertexArray = gl.createVertexArray();
     gl.bindVertexArray(vertexArray);
     vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new this.Float32Array(vertices), gl.STATIC_DRAW);
+    indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER,indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,new Uint32Array(indices),gl.STATIC_DRAW);
 
+    checkTexture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D,checkTexture);
+    gl.texImage2D(gl.TEXTURE_2D,0,gl.RGBA,2,2,0,gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array(texPixels));
+    gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_S,gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_T,gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_MIN_FILTER,gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_MAG_FILTER,gl.LINEAR)
     positionAttribID = gl.getAttribLocation(shaderProgram, "position");
+    uvCoordID = gl.getAttribLocation(shaderProgram, "uvCoordinate");
     gl.enableVertexAttribArray(positionAttribID);
-    gl.vertexAttribPointer(positionAttribID, 3, gl.FLOAT, gl.FALSE, 0, 0);
+    gl.enableVertexAttribArray(uvCoordID);
+    gl.vertexAttribPointer(positionAttribID, 3, gl.FLOAT, gl.FALSE, 20, 0);
+    gl.vertexAttribPointer(uvCoordID,2,gl.FLOAT,gl.FALSE,20,12)
 
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
-
+    //gl.drawArrays(gl.TRIANGLES, 0, 3);
+    //gl.drawArrays(gl.TRIANGLES,0,3);
+    gl.drawElements(gl.TRIANGLES,6,gl.UNSIGNED_INT,0);
 }
